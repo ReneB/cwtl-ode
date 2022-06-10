0ea38bebd9dc2e5356679d2996cf7bef90c62e9c
René van den Berg <rene@ranj.nl>
2022-06-10 19:32:32 +0200

    When entering the country we're deploying to, also set the WiFi country for regulation

--- finalize-installation.sh
+++ finalize-installation.sh
@@ -11,9 +11,9 @@ reconfigure_hostname() {
         return 1
     fi
 
-    read -e -p "Please enter the name of the country this device will be deployed to: " COUNTRY_CODE
+    read -e -p "Please enter the name of the country this device will be deployed to: " COUNTRY_NAME
 
-    echo "Will set hostname to $COUNTRY_CODE.cantwaittolearn-mdm.com everywhere."
+    echo "Will set hostname to $COUNTRY_NAME.cantwaittolearn-mdm.com everywhere."
 
     echo
 
@@ -21,12 +21,44 @@ reconfigure_hostname() {
     for TEMPLATE in $TEMPLATES; do
         TARGET=$(jq -r ".templates[\"$TEMPLATE\"]" hostname-mapping.json)
 
-	BASENAME=$(basename $TARGET)
-	echo "* Backing up $TARGET to hostname-backups/$BASENAME"
-	cp $TARGET hostname-backups/$BASENAME
-        echo "* Setting hostname to $COUNTRY_CODE.cantwaittolearn-mdm.com in $TARGET"
-	sed -e "s/REPLACEWITHCOUNTRY/$COUNTRY_CODE/" hostname-file-templates/$TEMPLATE > $TARGET
+        BASENAME=$(basename $TARGET)
+        echo "* Backing up $TARGET to hostname-backups/$BASENAME"
+        #cp $TARGET hostname-backups/$BASENAME
+        echo "* Setting hostname to $COUNTRY_NAME.cantwaittolearn-mdm.com in $TARGET"
+        #sed -e "s/REPLACEWITHCOUNTRY/$COUNTRY_NAME/" hostname-file-templates/$TEMPLATE > $TARGET
     done
+
+    COUNTRY_UPPER_CASE=$(echo "${COUNTRY_NAME^^}")
+    case $COUNTRY_UPPER_CASE in
+        UGANDA)
+            COUNTRY_CODE="UG"
+            ;;
+        LEBANON)
+            COUNTRY_CODE="LB"
+            ;;
+        CHAD)
+            COUNTRY_CODE="TD"
+            ;;
+        UKRAINE)
+            COUNTRY_CODE="UA"
+            ;;
+        SUDAN)
+            COUNTRY_CODE="SD"
+            ;;
+        JORDAN)
+            COUNTRY_CODE="JO"
+            ;;
+        "THE NETHERLANDS")
+            COUNTRY_CODE="NL"
+            ;;
+        *)
+            read -e -p "Please enter the 2-letter country code for ${COUNTRY_NAME} (needed for WiFi configuration regulation)" COUNTRY_CODE
+            ;;
+    esac
+
+    echo "Setting WiFi Country Config to ${COUNTRY_CODE}..."
+    iw reg set $COUNTRY_CODE
+    raspi-config nonint do_wifi_country $COUNTRY_CODE
 }
 
 reconfigure_hostname
